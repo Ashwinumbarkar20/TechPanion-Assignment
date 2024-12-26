a24ec37507a2c67e6382acc5222e4eae7f6e9ac5
Ashwin <Ashwin@vridhee.com>
2024-12-26 15:28:53 +0530

    add prd api created

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,6 +5,6 @@ const productmiddleware=require('../Middleware/validateId.middleware')
 
 
 router.get('/', productController.getAllProducts);
-router.post('/products', productmiddleware.validateProductData,productController.addProduct);
+router.post('/Addproducts', productmiddleware.validateProductData,productController.addProduct);
 
 module.exports = router;
